e250c6cbc09f41a1fa940fe99c6819ff06c333ec
choidong-oh <qltjfeo@naver.com>
2025-02-21 17:55:28 +0900

    폭탄 구현 완료
    이펙트 넣엇음

--- Assets/Scripts/Boom.cs
+++ Assets/Scripts/Boom.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boom : MonoBehaviour
+public class Boom : MonoBehaviour, IDamageable
 {
     Rigidbody rb;
     Vector3 velocity;
     bool isGrounded = false;
+    public GameObject explodePrefab; //Æø¹ß ÇÁ¸®ÆÕ
+    float radius = 5f;//Æø¹ß ¹üÀ§
+    public LayerMask toExplode;
+    int boomDagage = 1000;
 
     private void Start()
     {
@@ -32,9 +36,10 @@ public class Boom : MonoBehaviour
         //transform.position += velocity;
         rb.constraints = (RigidbodyConstraints)0;
         throwDirection = Camera.main.transform.forward;
-        rb.AddForce(throwDirection *1300);
+        //rb.AddForce(throwDirection *1300);
+        rb.AddForce(throwDirection *10);
         rb.useGravity = true;
-        //StartCoroutine(boomWait());
+        StartCoroutine(boomWait());
     }
     
    
@@ -51,8 +56,37 @@ public class Boom : MonoBehaviour
     {
 
         yield return new WaitForSeconds(3f);
-
-        //Destroy(gameObject);
+        Explode();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 
+
+
+    void Explode()
+    {
+        //Instantiate(explodePrefab, transform.position, Quaternion.identity);
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position,3,Vector3.up,0f,LayerMask.GetMask("Enemy"));
+        GameObject fish = Instantiate(explodePrefab) as GameObject;
+        fish.transform.SetParent(transform, false);
+        //Instantiate(explodePrefab,transform.position, Quaternion.identity); 
+        foreach (var hitobj in hits)
+        {
+            var dsd = hitobj.collider.GetComponent<IDamageable>();
+            dsd.GetDamge(boomDagage);
+            Debug.Log("°¨Áö‰Î");
+
+
+        }
+
+
+    }
+
+    public void GetDamge(int damage)
+    {
+
+
+
+
+    }
 }
